18caf5dfbcb6ada9fa20d8214af7bc771e35e7f7
전율 <jeon-yul@jeon-yul-ui-MacBookPro.local>
2023-03-02 21:59:18 +0900

    230302

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import evenOdd from "./pages/230228";
 import average from "./pages/230301";
+import add from "./pages/230302";
 
 function App() {
   return (
     <div>
       <div>{evenOdd(2)}</div>
       <div>{average([1, 2, 3, 4, 5, 6, 7, 8, 9])}</div>
+      <div>{add(10123542)}</div>
     </div>
   );
 }
