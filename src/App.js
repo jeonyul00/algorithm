385111a30c7d4c68a0acc28f48df915d0e6ebad3
전율 <jeon-yul@jeon-yul-ui-MacBookPro.local>
2023-03-09 23:00:03 +0900

    230309

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import average from "./pages/230301";
 import add from "./pages/230302";
 import divisor from "./pages/230303";
 import reverse from "./pages/230306";
+import distance from "./pages/230309";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div>{add(10123542)}</div>
       <div>{divisor(12)}</div>
       <div>{reverse(123456789)}</div>
+      <div>{distance(-4, 2)}</div>
     </div>
   );
 }
